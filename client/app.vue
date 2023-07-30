8cbeb68e79a4099b6f5c79585a16793e9c9adfc1
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2023-07-30 10:13:27 +0700

    create client project

---
+++ client/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style  scoped>
+
+</style>
---
+++ client/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import {resolve} from "path"
+export default defineNuxtConfig({
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  css: ["~/assets/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
