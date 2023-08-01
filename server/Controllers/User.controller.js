938065819e3b177ececaf4e2e225b47b4e43acc7
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2023-08-01 10:10:57 +0700

    add controller logout

--- server/Controllers/User.controller.js
+++ server/Controllers/User.controller.js
@@ -1,7 +1,8 @@
 const UserSchema = require('../Models/User.model')
 const createError = require('http-errors');
 const userValidate = require('../Helpers/validation');
-const {signAccessToken} = require('../Helpers/jwt_service')
+const client = require('../Helpers/connection_redis')
+const { signAccessToken, signRefreshToken, verifyRefreshToken} = require('../Helpers/jwt_service')
 
 module.exports = {
     fetchAll: async (req, res, next) => {
@@ -33,17 +34,17 @@ module.exports = {
             if (isExists) {
                 createError.Conflict(`${phoneNumber} is ready been registered`)
             }
-    
+
             //create new user
             const user = new UserSchema({
                 fullName,
                 phoneNumber,
                 password
             })
-    
+
             //save user into DB
             const savedUser = await user.save()
-    
+
             return res.json({
                 status: 'ok',
                 elements: savedUser
@@ -59,27 +60,77 @@ module.exports = {
             if (error) {
                 throw createError(error.details[0].message);
             }
+
             const { phoneNumber, password } = req.body;
-    
             const user = await UserSchema.findOne({
                 phoneNumber
             })
             if (!user) {
                 throw createError.NotFound(`${phoneNumber} is not registerd`)
             }
-    
+
             const isValid = await user.isCheckPassword(password)
             if (!isValid) {
                 throw createError.Unauthorized()
             }
-
             const accessToken = await signAccessToken(user._id);
+            const refreshToken = await signRefreshToken(user._id);
             res.json({
-                accessToken
+                accessToken,
+                refreshToken
             })
-    
+
         } catch (error) {
             next(error)
         }
+    },
+
+    refreshToken: async(req, res, next)=>{
+        try {
+            console.log(req.body);
+            const {refreshToken} = req.body;
+            if(!refreshToken) throw createError.BadRequest();
+            const userID = await verifyRefreshToken(refreshToken);
+            const newAccessToken = await signAccessToken(userID);
+            const newRefreshToken = await signRefreshToken(userID);
+
+            res.json({
+                newAccessToken,
+                newRefreshToken
+            })
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    logout: async (req, res, next) => {
+        try {
+            const {refreshToken} = req.body;
+            if(!refreshToken){
+                throw createError.BadRequest();
+            }
+            const {userID} = await verifyRefreshToken(refreshToken);
+            client.del(userID.toString(), (err, reply)=>{
+                if(err){
+                    throw createError.InternalServerError();
+                }
+                res.json({
+                    message: 'logout!'
+                })
+            })
+        } catch (error) {
+            next(error)
+        }
+    },
+    getListUser: (req, res, next) => {
+        console.log(req.headers);
+        const listUser = [
+            {
+                "email": "adb"
+            }
+        ]
+        res.json({
+            listUser
+        })
     }
 }
