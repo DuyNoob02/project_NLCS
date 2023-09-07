cc8179b4c2373fd1f149e7f56a726162ee5a1cff
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2023-09-07 19:26:56 +0700

    stateCheck when register and login

--- server/Helpers/validation.js
+++ server/Helpers/validation.js
@@ -1,12 +1,22 @@
 const Joi = require('joi')
 
-const userValidate = data =>{
+const userValidate = data => {
     const userSchema = Joi.object({
         fullName: Joi.string().lowercase(),
         phoneNumber: Joi.string().min(10).max(10).required(),
-        password: Joi.string().min(4).max(20).required()
+        password: Joi.string().min(4).max(20).required(),
+        role: Joi.string(),
+        email: Joi.string().email()
     })
     return userSchema.validate(data);
 }
 
-module.exports = userValidate;
+const userValidateLogin = data => {
+    const userSchema = Joi.object({
+        phoneNumber: Joi.string().min(10).max(10).required(),
+        password: Joi.string().min(4).max(20).required(),
+        role: Joi.string(),
+    })
+    return userSchema.validate(data);
+}
+module.exports = { userValidate, userValidateLogin };
