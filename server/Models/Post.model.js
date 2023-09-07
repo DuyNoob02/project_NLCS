a6a9ada31ab0a943294fbc6052dbe869e66e17c6
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2023-09-07 19:25:44 +0700

    update

--- server/Models/Post.model.js
+++ server/Models/Post.model.js
@@ -5,21 +5,61 @@ const Schema = mongoose.Schema;
 const { Connection } = require('../Helpers/connections_mongodb')
 
 const realEstateSchema = new Schema({
-    code: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    acreage: {type: String, required: true},
-    rooms: {
-        bedrooms: { type: Number },
-        bathrooms: { type: Number },
-        livingRooms: { type: Number },
-        // Các phòng khác...
+    code: {
+        type: String,
+        required: true,
+        unique: true
     },
-    amenities: {type: String},
-    price: { type: String, required: true },
-    type: { type: String, required: true },
-    description: { type: String },
-    images: [{ type: String }]
+    formality: {
+        type: String
+    },
+    userID: {
+        type: String,
+        // unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    acreage: {
+        type: String,
+        required: true
+    },
+    bedrooms: {
+        type: Number
+    },
+    bathrooms: {
+        type: Number
+    },
+    livingRooms: {
+        type: Number
+    },
+    amenities: {
+        type: String
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    images: [{
+        type: String
+    }],
+    pending: {
+        type: Boolean,
+        default: true
+    }
+
 })
 
 module.exports = Connection.model('Post', realEstateSchema);
