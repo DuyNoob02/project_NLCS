53099379b24f6ad5dc39ee70600863085d79da9e
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2023-09-07 19:23:47 +0700

    update

--- server/Routes/User.route.js
+++ server/Routes/User.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const route = express.Router()
 const UserController = require('../Controllers/User.controller');
-const {verifyAccessToken} = require('../Helpers/jwt_service')
-
+const { verifyAccessToken } = require('../Helpers/jwt_service')
+const userValidate = require('../Helpers/validation');
+const path = require('path');
+const User_uploadCloud = require('../Helpers/User_cloudinary')
 
 route.get('/', UserController.fetchAll);
 
@@ -11,8 +13,11 @@ route.post('/register', UserController.register)
 route.post('/login', UserController.login)
 
 route.delete('/logout', UserController.logout)
-
-route.get('/getlist',verifyAccessToken ,UserController.getListUser)
+route.delete('/deleteUser/:id', UserController.deleteUser)
+// route.get('/getInfoUser',verifyAccessToken ,UserController.getInfoUser)
+route.get('/getInfoUser/:id', UserController.getInfoUser)
+route.put('/update/:id', UserController.update)
+route.put('/updateAvt/:id', User_uploadCloud.single('image'), UserController.updateAVT)
 
 route.post('/refresh-token', UserController.refreshToken)
 module.exports = route
