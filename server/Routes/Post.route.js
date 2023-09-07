65d404abb3c06e5cc6b4c340030913db802bd67a
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2023-09-07 19:23:22 +0700

    Thay đổi phương thức lưu trữ từ disk lên clound

--- server/Routes/Post.route.js
+++ server/Routes/Post.route.js
@@ -3,28 +3,34 @@ const route = express.Router()
 const Post = require('../Controllers/Post.controller')
 const multer = require('multer')
 const path = require('path');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads');
-    },
-    filename: function (req, file, cb) {
-        console.log(file);
-        const timestamp = Date.now();
-        const extension = path.extname(file.originalname);
-        const fileName = `${timestamp}${extension}`;
-        cb(null, fileName);
-    }
-})
-const upload = multer({
-    storage: storage
-}).single('images')
+const {verifyAccessToken} = require('../Helpers/jwt_service');
+const uploadCloud = require('../Helpers/Post_cloudinary')
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './uploads');
+//     },
+//     filename: function (req, file, cb) {
+//         console.log(file);
+//         const timestamp = Date.now();
+//         const extension = path.extname(file.originalname);
+//         const fileName = `${timestamp}${extension}`;
+//         cb(null, fileName);
+//     }
+// })
+//  const  upload = multer({
+//     storage: storage
+// }).single('images')
 
 route.get('/', Post.getAllPost)
-route.get('/:id',Post.getPostByID)
-route.post('/', upload, Post.createPost)
-route.put('/:id', upload, Post.updatePost)
+route.get('/:id', Post.getPostByID)
+route.get('/getPost/:id', Post.getPostbyIdUser)
+route.post('/post', verifyAccessToken, uploadCloud.array('images'), Post.createPost)
+// route.post('/post', upload, Post.createPost)
+route.put('/:id', uploadCloud.array('images'), Post.updatePost)
 route.delete('/:id', Post.deletePost)
 
 module.exports = route;
+// module.exports = upload;
+
+
 
