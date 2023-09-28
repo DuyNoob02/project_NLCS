2fd8c727e890dfff2d7ad0fce33cbc11eb2a68d5
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2023-09-28 15:16:01 +0700

    create Chart component

---
+++ client/components/Chart.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h2 class="text-lg uppercase font-medium text-cyan-400">Thống kê lượng bài đăng</h2>
+        <canvas ref="chartCanvas"></canvas>
+    </div>
+</template>
+  
+<script setup>
+import Chart from 'chart.js/auto';
+import { onMounted, ref } from 'vue';
+
+const chartCanvas = ref(null);
+
+onMounted(async () => {
+    const { data: getPostData } = await useFetch('http://localhost:3001/api/admin/getChartData', {
+        method: "GET"
+    });
+
+    const ChartData = getPostData.value;
+    const labels = Object.keys(ChartData);
+    const dataValues = Object.values(ChartData);
+
+    const ctx = chartCanvas.value.getContext('2d');
+
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Số bài đăng',
+                data: dataValues,
+                // backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                // borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                borderRadius: 2
+            },
+        ],
+    };
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    max: Math.max(...dataValues) + 1,
+                },
+            },
+        },
+    });
+});
+
+</script>
+  
