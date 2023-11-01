64466a8bd2c956ee163db4aa853ec35697ac8774
DuyNoob02 <nguyenkhachuy25122002@gmail.com>
2023-11-01 15:38:50 +0700

    add controller

--- server/Routes/Admin.route.js
+++ server/Routes/Admin.route.js
@@ -2,12 +2,20 @@ const express = require('express')
 const route = express.Router()
 const AdminController = require('../Controllers/Admin.controller')
 
+route.get('/getAllUser', AdminController.fetchAll)
 route.get('/getPostPending', AdminController.getPostPending)
 route.get('/getChartData', AdminController.getPostSevendayAgo)
+route.get('/getAccount', AdminController.getAccountPending)
 route.get('/getPostDeleting', AdminController.getPostDelete)
+route.get('/countPostPerMonth', AdminController.countPostPerMonth)
+
 route.put('/markForDelete/:id', AdminController.markForDelete)
+route.put('/acceptUser/:id', AdminController.acceptUser)
 route.put('/returnPost/:id', AdminController.returnPost)
 route.put('/changeState/:id', AdminController.changeStatePending)
+
+route.post('/sendConfirmation', AdminController.sendConfirmationAccount)
+
 route.delete('/deleteUser/:id', AdminController.deleteUser)
 route.delete('/deletePost/:id', AdminController.deletePost)
 
